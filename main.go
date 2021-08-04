8e7c82c5591fd76d50c09622551c13ced34c6f11
BikramKumar6928 <bk6928@gmail.com>
2021-08-04 15:52:53 +0530

    Create implementation of functions

--- main.go
+++ main.go
@@ -1,29 +1,25 @@
 package main
 
 import (
+	"fmt"
+	"sync"
+
 	"github.com/emirpasic/gods/sets/hashset"
 )
 
 // *********************  Models begin  *********************
 
-type Topic struct {
-	topicId   string
-	topicName string
+type Subscription struct {
+	subscriptionId       string
+	topicId              string
+	subscriptionFunction func(message string)
 }
 
-func (topic Topic) New(topicId string, topicName string) Topic {
-	return Topic{topicId, topicName}
-}
-
-// **********************************************************
-
-type Subsciption struct {
-	subsciptionId   string
-	subsciptionName string
-}
-
-func (subsciption Subsciption) New(subsciptionId string, subsciptionName string) Subsciption {
-	return Subsciption{subsciptionId, subsciptionName}
+func (subscription Subscription) NewWithoutFunction(subscriptionId string, topicId string) Subscription {
+	return Subscription{
+		subscriptionId: subscriptionId,
+		topicId:        topicId,
+	}
 }
 
 // **********************************************************
@@ -31,10 +27,11 @@ func (subsciption Subsciption) New(subsciptionId string, subsciptionName string)
 type Message struct {
 	messageId      string
 	messageContent string
+	subscriptionId string
 }
 
-func (message Message) New(messageId string, messageContent string) Message {
-	return Message{messageId, messageContent}
+func (message Message) New(messageId string, messageContent string, subscriptionId string) Message {
+	return Message{messageId, messageContent, subscriptionId}
 }
 
 // *********************  Models end  *********************
@@ -42,49 +39,134 @@ func (message Message) New(messageId string, messageContent string) Message {
 // *********************  Pubsub begin  *********************
 
 type PubSub struct {
-	topicIdMap                 map[string]Topic
-	subsciptionMapList         map[string][]Subsciption
+	topicIdSet                 hashset.Set
+	subscriptionSetMap         map[string]hashset.Set
 	messageMapList             map[string][]Message
 	currentlyProcessedMessages hashset.Set
+	lock                       sync.Mutex
 }
 
-func (pubSub PubSub) CreateTopic() {
+func (pubSub *PubSub) Initialize() {
+	pubSub.topicIdSet = *hashset.New()
+}
+
+func (pubSub *PubSub) CreateTopic(topicId string) {
+	pubSub.topicIdSet.Add(topicId)
+	fmt.Printf("Added %v topic to system\n", topicId)
+}
+
+func (pubSub *PubSub) DeleteTopic(topicId string) {
+	pubSub.topicIdSet.Remove(topicId)
+	fmt.Printf("Removed %v topic from system\n", topicId)
+}
+
+// Under work
+func (pubSub *PubSub) AddSubscription(topicId string, subscriptionId string) {
+	newSubscription := Subscription{
+		subscriptionId: subscriptionId,
+		topicId:        topicId,
+	}
+	subscriptionSet := pubSub.subscriptionSetMap[topicId]
+	if subscriptionSet.Empty() {
+		subscriptionSet = *hashset.New()
+		pubSub.subscriptionSetMap[topicId] = subscriptionSet
+	}
+	// TODO: having issue in adding objects to map
+	// subscriptionSet.Add(newSubscription)
+	fmt.Printf("newSubscription: %v\n", newSubscription)
+	pubSub.subscriptionSetMap[topicId] = subscriptionSet
+	fmt.Printf("Added %v subscription to system", subscriptionId)
+}
+
+func (pubSub *PubSub) DeleteSubscription(subscriptionId string) {
+	for _, subscriptionSet := range pubSub.subscriptionSetMap {
+		subscriptionSet.Remove(subscriptionId)
+	}
+}
+
+// Under work
+func (pubSub *PubSub) Publish(topicId string, message string) {
+	subscriptionSet := pubSub.subscriptionSetMap[topicId]
+	for _, subscription := range subscriptionSet.Values() {
+		subscription = subscription.(Subscription)
+		pubSub.lock.Lock()
+		pubSub.currentlyProcessedMessages.Add(Message{
+			genUUID(),
+			message,
+			topicId,
+		})
+		defer pubSub.lock.Unlock()
+		// go subscription.subscriberFunc()
+		fmt.Printf("subscription: %v\n", subscription)
+	}
+}
+
+func (pubSub *PubSub) Retry() {
+	pubSub.lock.Lock()
+	defer pubSub.lock.Unlock()
+	for _, message := range pubSub.currentlyProcessedMessages.Values() {
+		message = message.(Message)
+		// subscriptionId := message.subscriptionId
+		// subscriberFunc := getSubscriptionFromSubscriptionId().subscriberFunc Create the function
+		// go subscriberFunc()
+	}
+}
+
+func genUUID() string {
+	return "random-uuid"
+}
+
+// Under work
+func (pubSub *PubSub) Subscribe(subscriptionId string, subscriberFunc func(message string)) {
+	for _, subscriptionSet := range pubSub.subscriptionSetMap {
+		for _, subscription := range subscriptionSet.Values() {
+			subscription = subscription.(Subscription)
+			// if subscription.subscriptionId == subscriptionId {
+			// 	subscription.subscriberFunc = subscriberFunc
+			// }
+			fmt.Printf("subscription: %v\n", subscription)
+		}
+	}
+}
+
+// Under work
+func (pubSub *PubSub) UnSubscribe(subscriptionId string) {
+	for _, subscriptionSet := range pubSub.subscriptionSetMap {
+		for _, subscription := range subscriptionSet.Values() {
+			subscription = subscription.(Subscription)
+			// if subscription.subscriptionId == subscriptionId {
+			// subscriptionSet.Remove(subscription)
+			// }
+			fmt.Printf("subscription: %v\n", subscription)
+		}
+	}
 
 }
 
-func (pubSub PubSub) DeleteTopic() {
-
-}
-
-func (pubSub PubSub) AddSubscription() {
-
-}
-
-func (pubSub PubSub) DeleteSubscription() {
-
-}
-
-func (pubSub PubSub) Publish() {
-
-}
-
-func (pubSub PubSub) Subscribe() {
-
-}
-
-func (pubSub PubSub) UnSubscribe() {
-
-}
-
-func (pubSub PubSub) Ack() {
-
+func (pubSub *PubSub) Ack(subscriptionId string, messageId string) {
+	pubSub.lock.Lock()
+	pubSub.currentlyProcessedMessages.Remove(messageId)
+	defer pubSub.lock.Unlock()
 }
 
 // *********************  Pubsub end  *********************
 
-// *********************  Main  *********************
+// *********************  Main ****************************
 
 func main() {
 	pubSub := PubSub{}
-	pubSub.CreateTopic()
+	pubSub.Initialize()
+	pubSub.CreateTopic("topic1")
+	fmt.Println(pubSub)
+	pubSub.DeleteTopic("topic1")
+	fmt.Println(pubSub)
+
+	pubSub.CreateTopic("topic1")
+	fmt.Println(pubSub)
+	pubSub.AddSubscription("topic1", "subscription1")
+	fmt.Println(pubSub)
+
+	pubSub.Subscribe("subscription1", func(message string) {
+		fmt.Printf("message: %v\n", message)
+	})
 }
